cf6da630f2d35e52bd39f9d15a51a021a84cc8f4
TH_PHEC <59645009+gimon0330@users.noreply.github.com>
2020-11-13 22:05:06 +0900

    도움말 명령어 소폭 수정

--- exts/help.py
+++ exts/help.py
@@ -1,4 +1,4 @@
-import discord,os,json,random,datetime,requests,bs4
+import discord
 from discord.ext import commands 
 
 def get_embed(title, description='', color=0xCCFFFF):
@@ -14,40 +14,37 @@ class _help(commands.Cog):
         embed=get_embed("\📌 | **RTBOT 명령어**")
         embed.add_field(name='📝 | **정보**',value='`알티야 정보`,`알티야 프사`,`알티야 유저`,`알티야 프로필`,`알티야 핑`,`알티야 공지채널`',inline=False)
         embed.add_field(name='💰 | **도박**',value='`알티야 도박`,`알티야 저금`,`알티야 인출`,`알티야 내돈`,`알티야 은행잔고`,`알티야 돈내놔`,`알티야 돈줘`',inline=False)
-        embed.add_field(name='🌐 | **크롤링**',value='`알티야 빌보드`,`알티야 데몬`,`알티야 실검`,`알티야 노래순위`,`알티야 코로나`',inline=False)
         embed.add_field(name='💻 | **미니게임**',value='`알티야 강화 (물품/목록/삭제/판매)`,`알티야 가위바위보`,`알티야 슬롯`',inline=False)
-        embed.add_field(name='❓ | **도움말**',value='`알티야 도움 (정보,도박,크롤링,관리,미니게임)`',inline=False)
+        embed.add_field(name='❓ | **도움말**',value='`알티야 도움 (정보,도박,미니게임)`',inline=False)
         await ctx.send(embed=embed)
     
     @_help.command(name='정보')
     async def _help_1(self, ctx):
         embed=get_embed("\📝 | **정보 명령어**","""**알티야 정보** : 알티봇의 정보를 보여줍니다
-        **알티야 프사** : 멘션한 사람의 프사를 가져옵니다 (비우면 본인)
-        **알티야 유저** : 알티봇의 게임중인 유저수를 보여줍니다.
-        **알티야 프로필** : 멘션한 사람의 프로필을 가져옵니다 (비우면 본인)
-        **알티야 핑** : 알티봇의 핑을 보여줍니다.
-        **알티야 공지채널** : 알티봇의 전체 공지채널을 설정합니다.""")
+**알티야 프사** : 멘션한 사람의 프사를 가져옵니다 (비우면 본인)
+**알티야 유저** : 알티봇의 게임중인 유저수를 보여줍니다.
+**알티야 프로필** : 멘션한 사람의 프로필을 가져옵니다 (비우면 본인)
+**알티야 핑** : 알티봇의 핑을 보여줍니다.
+**알티야 공지채널** : 알티봇의 전체 공지채널을 설정합니다.""")
         await ctx.send(embed=embed)
 
     @_help.command(name='도박')
     async def _help_2(self, ctx):
         embed=get_embed("\📝 | **도박 명령어**","""**알티야 도박 (액수 / 올인)** : 도박을 시작합니다.
-        **알티야 저금 (액수 / 올인)** : 현재 모은 돈을 은행에 저금합니다.
-        **알티야 인출 (액수 / 올인)** : 저금된 돈을 꺼내옵니다.
-        **알티야 내돈** : 현재 잔액을 표시합니다.
-        **알티야 은행잔고** : 저금한 금액을 확인합니다.
-        **알티야 돈내놔** : 알티봇이 돈을 줍니다.
-        **알티야 돈줘 (@멘션) (액수)** : 멘션 한 사람에게 일정량의 돈을 줍니다.""")
-        await ctx.send(embed=embed)
-
-    @_help.command(name='크롤링')
-    async def _help_3(self, ctx):
-        embed=get_embed("\📝 | **크롤링 명령어**","""""")
+**알티야 저금 (액수 / 올인)** : 현재 모은 돈을 은행에 저금합니다.
+**알티야 인출 (액수 / 올인)** : 저금된 돈을 꺼내옵니다.
+**알티야 내돈** : 현재 잔액을 표시합니다.
+**알티야 은행잔고** : 저금한 금액을 확인합니다.
+**알티야 돈내놔** : 알티봇이 돈을 줍니다.
+**알티야 돈줘 (@멘션) (액수)** : 멘션 한 사람에게 일정량의 돈을 줍니다.""")
         await ctx.send(embed=embed)
 
     @_help.command(name='미니게임')
     async def _help_4(self, ctx):
-        embed=get_embed("\📝 | **미니게임 명령어**","""""")
+        embed=get_embed("\📝 | **미니게임 명령어**","""**알티야 숫자맞추기** : 숫자맞추기 게임을 합니다. 
+**알티야 가위바위보 (빠,묵,찌)** : 가위바위보를 합니다 이긴사람이 1000원을 갖습니다.
+**알티야 슬롯 (액수)** 슬롯게임을 합니다.
+**알티야 낚시** : 낚시를 합니다.""")
         await ctx.send(embed=embed)
 
 def setup(client):
