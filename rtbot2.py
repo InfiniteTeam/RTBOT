853df0575176223462d3256009b948cf0534268d
TH_PHEC <59645009+gimon0330@users.noreply.github.com>
2020-12-21 23:27:04 +0900

    3.6 알게디 (Algedi) Update

--- rtbot2.py
+++ rtbot2.py
@@ -1,7 +1,24 @@
-import discord,os
+import discord, os, asyncio, aiomysql
 from discord.ext import commands
 
-client = commands.AutoShardedBot(command_prefix=['알티야 ','ㅇ!','알','알 '])
+loop = asyncio.get_event_loop()
+
+async def connect_db():
+    pool = await aiomysql.create_pool(
+        host='arpa.kro.kr',
+        user='rtbot',
+        password='infinite2018!!rtbot',
+        db='rtbot',
+        charset='utf8',
+        autocommit=True
+    )
+    return pool
+    
+pool = loop.run_until_complete(connect_db())
+
+intents = discord.Intents.all()
+client = commands.AutoShardedBot(command_prefix=['알티야 ','ㅇ!','알','알 '],intents=intents)
+client.pool = pool
 
 for ext in filter(lambda x: x.endswith('.py') and not x.startswith('_'), os.listdir('./exts')):
     client.load_extension('exts.' + os.path.splitext(ext)[0])
