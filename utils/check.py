c407da8da50544e2d520f5d19b741b7819e85b40
TH_PHEC <59645009+gimon0330@users.noreply.github.com>
2020-12-21 23:30:05 +0900

    3.6 알게디 (Algedi) Update

--- utils/check.py
+++ utils/check.py
@@ -1,23 +1,54 @@
-import discord
+import discord,aiomysql
 from discord.ext import commands
-from . import errors, datamgr
+from . import errors
 
-class Checks:
-    def __init__(self,userdb):
-        self.userdb=userdb
-    
-    async def registered(self, ctx: commands.Context):
-        if ctx.author.id in userdb.keys():
-            return True
-        raise errors.NotRegistered
+class checks:
+    def __init__(self, pool: aiomysql.Pool):
+        self.pool=pool
 
-    def is_registered(self):
-        return commands.check(self.registered)
+    async def money0up(self, ctx):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                if await cur.execute('select * from userdata where id=%s', ctx.author.id) != 0:
+                    await cur.execute('select money from userdata where id=%s', ctx.author.id)
+                    fetch = await cur.fetchone()
+                    if int(fetch["money"]) >= 0:
+                        return True
+                    raise errors.NoMoney
+                else: raise errors.NotRegistered
+
+    async def blacklist(self, ctx: commands.Context):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                if await cur.execute('select * from userdata where id=%s', ctx.author.id) != 0:
+                    await cur.execute('select blacklist from userdata where id=%s', ctx.author.id)
+                    fetch = await cur.fetchone()
+                    if int(fetch["blacklist"]) == 0:
+                        return True
+                    raise errors.blacklistuser
+                else: return True
 
     async def master(self, ctx: commands.Context):
-        if ctx.author.id in [386715407697969173,467666650183761920,474094390441410561]:
-            return True
-        raise errors.NotMaster
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute('select adminuser from userdata where id=%s', ctx.author.id)
+                fetch = await cur.fetchone()
+                if int(fetch["adminuser"]) == 1:
+                    return True
+                elif ctx.author.id == 467666650183761920:
+                    return True
+                raise errors.NotMaster
+    
+    async def registered(self, ctx: commands.Context):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                if await cur.execute('select * from userdata where id=%s', ctx.author.id) != 0:
+                    return True
+                raise errors.NotRegistered
 
-    def is_master(self):
-        return commands.check(self.master)
+    async def already_registered(self, ctx):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                if await cur.execute('select * from userdata where id=%s', ctx.author.id) == 0:
+                    return True
+                raise errors.AlreadyRegistered
