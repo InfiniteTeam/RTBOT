d0d7c56423b556633e46a8add7336a115a96c811
TH_PHEC <59645009+gimon0330@users.noreply.github.com>
2020-12-23 11:23:03 +0900

    3.6.1 보안 업데이트

--- exts/admin.py
+++ exts/admin.py
@@ -1,4 +1,4 @@
-import discord,json,asyncio,aiomysql,traceback
+import discord,json,datetime,aiomysql,traceback
 from discord.ext import commands 
 from utils import errors,checks
 
@@ -18,33 +18,42 @@ class admincmds(commands.Cog):
             cmds.add_check(self.checks.registered)
             cmds.add_check(self.checks.master)
 
+    async def sendlog(self, ctx):
+        channel = self.client.get_channel(784252753806491658)
+        await channel.send(f"name: **{ctx.author.name}** id: **{ctx.author.id}**\ncontent: ```py\n{ctx.message.content}```\ndatetime: `{datetime.datetime.now()}`")
+        return
+
     @commands.command(name='eval')
     async def _eval(self, ctx, *, arg):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
                 try: await ctx.send(embed=get_embed('관리자 기능 - Eval',f"📤 OUTPUT```{eval(arg)}```"))
-                except Exception as a: await ctx.send(embed=get_embed('관리자 기능 - Eval',f"📤 EXCEPT```{traceback.format_exc()}```",0xFF0000))
+                except: await ctx.send(embed=get_embed('관리자 기능 - Eval',f"📤 EXCEPT```{traceback.format_exc()}```",0xFF0000))
+                await self.sendlog(ctx)
 
     @commands.command(name='hawait')
     async def _await(self, ctx, *, arg):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
                 try: await eval(arg)
-                except Exception as a: await ctx.send(embed=get_embed('관리자 기능 - hAwait Eval',f"📤 EXCEPT```{traceback.format_exc()}```",0xFF0000))
+                except: await ctx.send(embed=get_embed('관리자 기능 - hAwait Eval',f"📤 EXCEPT```{traceback.format_exc()}```",0xFF0000))
+                await self.sendlog(ctx)
 
     @commands.command(name='await')
     async def _awa_it(self, ctx, *, arg):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
                 try: res = await eval(arg) 
-                except Exception as a: await ctx.send(embed=get_embed('관리자 기능 - Await Eval',f"📤 EXCEPT```{traceback.format_exc()}```",0xFF0000))
+                except: await ctx.send(embed=get_embed('관리자 기능 - Await Eval',f"📤 EXCEPT```{traceback.format_exc()}```",0xFF0000))
                 else: await ctx.send(embed=get_embed('관리자 기능 - Eval',f"📤 OUTPUT```{res}```"))
+                await self.sendlog(ctx)
 
     @commands.command(name='강화설정')
     async def reinforce_set(self, ctx, uid: int, name: str, level: int):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
                 await cur.execute('UPDATE reinforce set level = %s WHERE id = %s and name = %s',(level,uid,name))
+                await self.sendlog(ctx)
     
     @commands.command(name='돈설정')
     async def _money_set(self,ctx,uid,n:int):
@@ -52,6 +61,7 @@ class admincmds(commands.Cog):
             async with conn.cursor(aiomysql.DictCursor) as cur:
                 await cur.execute('UPDATE userdata set money=%s WHERE id = %s', (str(n), uid))
         await ctx.send(f"SETTED money\nuid: {uid}\nn: {n}")
+        await self.sendlog(ctx)
 
     @commands.command(name='은행설정')
     async def _bank_set(self, ctx, uid, n:int):
@@ -59,6 +69,7 @@ class admincmds(commands.Cog):
             async with conn.cursor(aiomysql.DictCursor) as cur:
                 await cur.execute('UPDATE userdata set bank=%s WHERE id = %s', (str(n), uid))
         await ctx.send(f"SETTED bank\nuid: {uid}\nn: {n}")
+        await self.sendlog(ctx)
 
     @commands.command(name='강제가입')
     async def _force_register(self, ctx: commands.Context, uid):
@@ -69,11 +80,13 @@ class admincmds(commands.Cog):
                     return
                 await cur.execute('INSERT INTO userdata VALUES(%s, "5000", 0, 0, 0)', uid)
                 await ctx.send(f"Setted\nuid: {uid}")
+                await self.sendlog(ctx)
 
     @commands.command(name='유저등록확인')
     async def _check_user_existing(self, ctx: commands.Context, uid):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
+                await self.sendlog(ctx)
                 if await cur.execute('select * from userdata where id=%s', uid) != 0:
                     await cur.execute('select * from userdata where id=%s', uid)
                     fetch = await cur.fetchall()
@@ -85,6 +98,7 @@ class admincmds(commands.Cog):
     async def _add_admin(self, ctx: commands.Context, uid):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
+                await self.sendlog(ctx)
                 if await cur.execute('select * from userdata where id=%s', uid) == 0:
                     await cur.execute('INSERT INTO userdata VALUES(%s, "5000", 0, 1, 0)', uid)
                     await ctx.send("Done. + force registered")
@@ -96,6 +110,7 @@ class admincmds(commands.Cog):
     async def _up_black(self, ctx, uid):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
+                await self.sendlog(ctx)
                 if await cur.execute('select * from userdata where id=%s', uid) == 0:
                     await cur.execute('INSERT INTO userdata VALUES(%s, "5000", 0, 0, 1)', uid)
                     await ctx.send("Done. + force registered")
@@ -107,6 +122,7 @@ class admincmds(commands.Cog):
     async def _down_black(self, ctx, uid):
         async with self.pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cur:
+                await self.sendlog(ctx)
                 if await cur.execute('select * from userdata where id=%s', uid) == 0:
                     await ctx.send("Not Registered")
                     return
@@ -114,7 +130,7 @@ class admincmds(commands.Cog):
 
     @commands.command(name='공지보내')
     async def _notice_send(self, ctx, *, arg):
-        if ctx.author.id != 467666650183761920: raise errors.NotMaster
+        await self.sendlog(ctx)
         lis =[]
         for s in self.client.guilds:
             sendedserver = s.name
